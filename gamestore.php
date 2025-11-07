64f5c6b45b3d1b222cf9e0be8f12f7b7b1ebf30d
RainyUnnr2 <169143838+Valerioconti42@users.noreply.github.com>
2025-11-07 11:41:56 +0100

    MODIFICATO GAMESTORE.php

--- gamestore.php
+++ gamestore.php
@@ -1,3 +1,11 @@
 <?php
   ini_set("display_errors", 1);
+  function test_input($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+  $cat= test_input($_POST["categoria"]);
+  $catarr= array("sport"=>array(),"fps"=>array(),"idle"=>array(),"mp"=>array(),"indie"=>array(),"adv"=>array());
 ?>
